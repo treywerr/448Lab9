591a698ddb6282587375775b3e5965ecb841c9d3
Trey Werr <treywerr@gmail.com>
2020-11-14 16:57:53 -0600

    Final cleanup

--- customerBackend.php
+++ customerBackend.php
@@ -20,7 +20,7 @@ $totalCost = $appleCost+$bananaCost+$orangeCost+$shippingCost;
 echo '<link href="style.css" rel="stylesheet" type="text/css"/>'; 
 
 echo "<h2>Welcome " . $user . "<h2>";
-echo "Your password is " . $pass . "<br>";
+echo "<p>Your password is " . $pass . "<p><br>";
 echo "<br><table>";
 echo "<tr><th></th><th>Quantity</th><th>Cost Per Item</th><th>Subtotal</th></tr>";
 echo "<tr><th>Apples</th><td>" . $apples . "</td><td>$1.50</td><td>$" . $apples*1.5 . "</td></tr>";
