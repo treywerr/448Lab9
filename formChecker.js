52fcf1824fca97d6a6f945bef9149de06dd4d3af
Trey Werr <treywerr@gmail.com>
2020-11-14 14:07:52 -0600

    inital commit ex3

---
+++ formChecker.js
@@ -0,0 +1,13 @@
+function checkForms() {
+    let a = document.getElementById('apple').value;
+    let b = document.getElementById('banana').value;
+    let o = document.getElementById('orange').value;
+
+    alert("test");
+    if (a == "") {
+        alert("true");
+        return false;
+    }
+
+    return false;
+}
